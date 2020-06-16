4aa77b291a602e902a2b02cb1ee5aa9557a19a75
Amelia Krogh <ameliakrogh@gmail.com>
2020-06-16 14:53:52 -0400

    Created a function that displays a list of the token inside the chaos bag, depending on selected difficulty

--- app.js
+++ app.js
@@ -4,14 +4,34 @@ let standardTokenBag = ["+1", "0", "0", "-1", "-1", "-1", "-2", "-2", "-3", "-4"
 let hardTokenBag = ["0", "0", "0", "-1", "-1", "-2", "-2", "-3", "-3", "-4", "-5", "skull", "skull", "mask", "broken tablet", "tentacles", "star"]
 let expertTokenBag = ["0", "-1", "-1", "-2", "-2", "-3", "-3", "-4", "-4", "-5", "-6", "-8", "skull", "skull", "mask", "broken tablet", "tentacles", "star"]
 
-//TODO: display the array of tokens to the user on screen once they select a difficulty
+//Select main container to add new elements to
+const divContainer = document.querySelector('.container');
+const newDiv = document.createElement('div');
+
+//Function that creates a new div that contains the array of tokens in selected chaos bag
+function createDiv(){
+    divContainer.appendChild(newDiv);
+}
+
+//Display the relevant array of tokens to the user on screen once they select a difficulty
 const difficultySelector = document.getElementById("difficulty-select");
 difficultySelector.addEventListener('input', function displayTokens(event){
-    console.log(event.target.value);
-    if (event.target.value === "Easy") {
-        console.log(easyTokenBag);
-    }
-});
+    createDiv();
+    //Use the Switch statement to evaluate the difficulty value selected
+    switch (event.target.value) {
+            case "Easy":
+                newDiv.textContent = easyTokenBag;
+                break;
+            case "Standard":
+                newDiv.textContent = standardTokenBag;
+                break;
+            case "Hard":
+                newDiv.textContent = hardTokenBag;
+                break;
+            case "Expert":
+                newDiv.textContent = expertTokenBag;
+        }
+    });
 
 //TODO: add listener event for 'draw chaos token' button and randomly select an item from the set difficulty chaos bag
 //TODO: display the last drawn token to the user if they keep drawing tokens
