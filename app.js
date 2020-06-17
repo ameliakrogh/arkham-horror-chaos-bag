2a2d4330a80a7a2395d399c0ab7c073e7d3281cd
Amelia Krogh <ameliakrogh@gmail.com>
2020-06-17 10:54:07 -0400

    Adjusted both functions to create new divs and display token information based on specific event cases

--- app.js
+++ app.js
@@ -6,30 +6,26 @@ let expertTokenBag = ["0", "-1", "-1", "-2", "-2", "-3", "-3", "-4", "-4", "-5",
 
 //Select main container to add new elements to
 const divContainer = document.querySelector('.container');
-const newDiv = document.createElement('div');
-
-//Function that creates a new div that contains the array of tokens in selected chaos bag
-function createDiv(){
-    divContainer.appendChild(newDiv);
-}
+const tokenBagDiv = document.createElement('div');
+const displayTokenDiv = document.createElement('div');
 
 //Display the relevant array of tokens to the user on screen once they select a difficulty
 const difficultySelector = document.getElementById("difficulty-select");
 difficultySelector.addEventListener('input', function displayTokens(event){
-    createDiv();
+    divContainer.appendChild(tokenBagDiv);
     //Use the Switch statement to evaluate the difficulty value selected
     switch (event.target.value) {
             case "Easy":
-                newDiv.textContent = easyTokenBag;
+                tokenBagDiv.textContent = easyTokenBag;
                 break;
             case "Standard":
-                newDiv.textContent = standardTokenBag;
+                tokenBagDiv.textContent = standardTokenBag;
                 break;
             case "Hard":
-                newDiv.textContent = hardTokenBag;
+                tokenBagDiv.textContent = hardTokenBag;
                 break;
             case "Expert":
-                newDiv.textContent = expertTokenBag;
+                tokenBagDiv.textContent = expertTokenBag;
         }
     });
 
@@ -38,22 +34,24 @@ function randomToken(tokenBag) {
     return tokenBag[Math.floor(Math.random() * tokenBag.length)];
 }
 
-//TODO: add listener event for 'draw chaos token' button and randomly select an item from the set difficulty chaos bag
+//Depending on difficulty selected, randomly select a token when 'draw chaos token' button is clicked
 const drawTokenButton = document.getElementById("draw-token-button");
 drawTokenButton.addEventListener('click', function displayAToken(){
+    difficultySelector.after(displayTokenDiv);
     switch (difficultySelector.value) {
         case "Easy":
-            console.log(randomToken(easyTokenBag));
+            displayTokenDiv.textContent = randomToken(easyTokenBag);
             break;
         case "Standard":
-            console.log(randomToken(standardTokenBag));
+            displayTokenDiv.textContent = randomToken(standardTokenBag);
             break;
         case "Hard":
-            console.log(randomToken(hardTokenBag));
-                break;
+            displayTokenDiv.textContent = randomToken(hardTokenBag);
+            break;
         case "Expert":
-            console.log(randomToken(expertTokenBag));
+            displayTokenDiv.textContent = randomToken(expertTokenBag);
     }
 });
 
+//TODO: display to the user, the randomly selected token (instead of cconsole logging it)
 //TODO: display the last drawn token to the user if they keep drawing tokens
